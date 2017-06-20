2c4b7df2dbd93c18d1e246661c9df9b9a015c1cc
Taufiq <dhanani94@gmail.com>
2017-06-20 08:11:26 -0500

    separating out different components

--- game.py
+++ game.py
@@ -1,290 +1,55 @@
-from collections import deque, namedtuple
+from collections import deque
 import random
 import pygame
-import select
+from snake import Snake
+import utils
 
-BOARD_LENGTH = 32
-OFFSET = 16
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
+SNAKE_SPEED = 3
 
-
-DIRECTIONS = namedtuple('DIRECTIONS',
-        ['Up', 'Down', 'Left', 'Right'])(0, 1, 2, 3)
-
-def rand_color():
-    return (random.randrange(254)|64, random.randrange(254)|64, random.randrange(254)|64)
-
-class Snake(object):
-    def __init__(self, direction=DIRECTIONS.Right, 
-            point=(0, 0, rand_color()), color=None):
-        self.tailmax = 4
-        self.direction = direction 
-        self.deque = deque()
-        self.deque.append(point)
-        self.color = color
-        self.nextDir = deque()
-    
-    def get_color(self):
-        if self.color is None:
-            return rand_color()
-        else:
-            return self.color
-    
-    def populate_nextDir(self, events, identifier):
-        if (identifier == "arrows"):
-            for event in events:
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        self.nextDir.appendleft(DIRECTIONS.Up)
-                    elif event.key == pygame.K_DOWN:
-                        self.nextDir.appendleft(DIRECTIONS.Down)
-                    elif event.key == pygame.K_RIGHT:
-                        self.nextDir.appendleft(DIRECTIONS.Right)
-                    elif event.key == pygame.K_LEFT:
-                        self.nextDir.appendleft(DIRECTIONS.Left)
-        if (identifier == "wasd"):
-            for event in events:
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_w:
-                        self.nextDir.appendleft(DIRECTIONS.Up)
-                    elif event.key == pygame.K_s:
-                        self.nextDir.appendleft(DIRECTIONS.Down)
-                    elif event.key == pygame.K_d:
-                        self.nextDir.appendleft(DIRECTIONS.Right)
-                    elif event.key == pygame.K_a:
-                        self.nextDir.appendleft(DIRECTIONS.Left)
-
-
-def find_food(spots):
-    while True:
-        food = random.randrange(BOARD_LENGTH), random.randrange(BOARD_LENGTH)
-        if (not (spots[food[0]][food[1]] == 1 or
-            spots[food[0]][food[1]] == 2)):
-            break
-    return food
-
-
-def end_condition(board, coord):
-    if (coord[0] < 0 or coord[0] >= BOARD_LENGTH or coord[1] < 0 or
-            coord[1] >= BOARD_LENGTH):
-        return True
-    if (board[coord[0]][coord[1]] == 1):
-        return True
-    return False
-
-def make_board():
-    spots = [[] for i in range(BOARD_LENGTH)]
-    for row in spots:
-        for i in range(BOARD_LENGTH):
-            row.append(0)
-    return spots
-    
-
-def update_board(screen, snakes, food):
-    rect = pygame.Rect(0, 0, OFFSET, OFFSET)
-
-    spots = [[] for i in range(BOARD_LENGTH)]
-    num1 = 0
-    num2 = 0
-    for row in spots:
-        for i in range(BOARD_LENGTH):
-            row.append(0)
-            temprect = rect.move(num1 * OFFSET, num2 * OFFSET)
-            pygame.draw.rect(screen, BLACK, temprect)
-            num2 += 1
-        num1 += 1
-    spots[food[0]][food[1]] = 2
-    temprect = rect.move(food[1] * OFFSET, food[0] * OFFSET)
-    pygame.draw.rect(screen, rand_color(), temprect)
-    for snake in snakes:
-        for coord in snake.deque:
-            spots[coord[0]][coord[1]] = 1
-            temprect = rect.move(coord[1] * OFFSET, coord[0] * OFFSET)
-            pygame.draw.rect(screen, coord[2], temprect)
-    return spots
-
-def get_color(s):
-    if s == "bk":
-        return BLACK
-    elif s == "wh":
-        return WHITE
-    elif s == "rd":
-        return RED
-    elif s == "bl":
-        return BLUE
-    elif s == "fo":
-        return rand_color()
-    else:
-        print("WHAT", s)
-        return BLUE
-
-# Return 0 to exit the program, 1 for a one-player game
-def menu(screen):
-    font = pygame.font.Font(None, 30)
-    menu_message1 = font.render("Press enter for one-player, t for two-player", True, WHITE)
-    menu_message2 = font.render("Red is first player, blue is second player", True, WHITE)
-
-    screen.fill(BLACK)
-    screen.blit(menu_message1, (32, 32)) 
-    screen.blit(menu_message2, (32, 64))
-    pygame.display.update()
-    while True: 
-        done = False
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    return 1
-                if event.key == pygame.K_t:
-                    return 2
-                if event.key == pygame.K_l:
-                    return 3
-                if event.key == pygame.K_n:
-                    return 4
-        if done:
-            break
-    if done:
-        pygame.quit()
-        return 0
-
-def quit(screen):
-    return False
-
-def move(snake):
-    if len(snake.nextDir) != 0:
-        next_dir = snake.nextDir.pop()
-    else:
-        next_dir = snake.direction
-    head = snake.deque.pop()
-    snake.deque.append(head)
-    next_move = head
-    if (next_dir == DIRECTIONS.Up):
-        if snake.direction != DIRECTIONS.Down:
-            next_move =  (head[0] - 1, head[1], snake.get_color())
-            snake.direction = next_dir
-        else:
-            next_move =  (head[0] + 1, head[1], snake.get_color())
-    elif (next_dir == DIRECTIONS.Down):
-        if snake.direction != DIRECTIONS.Up:
-            next_move =  (head[0] + 1, head[1], snake.get_color())
-            snake.direction = next_dir
-        else:
-            next_move =  (head[0] - 1, head[1], snake.get_color())
-    elif (next_dir == DIRECTIONS.Left):
-        if snake.direction != DIRECTIONS.Right:
-            next_move =  (head[0], head[1] - 1, snake.get_color())
-            snake.direction = next_dir
-        else:
-            next_move =  (head[0], head[1] + 1, snake.get_color())
-    elif (next_dir == DIRECTIONS.Right):
-        if snake.direction != DIRECTIONS.Left:
-            next_move =  (head[0], head[1] + 1, snake.get_color())
-            snake.direction = next_dir
-        else:
-            next_move =  (head[0], head[1] - 1, snake.get_color())
-    return next_move
-
-def is_food(board, point):
-    return board[point[0]][point[1]] == 2
-
-def one_player(screen): 
+def initialize_game():
     clock = pygame.time.Clock()
-    spots = make_board()
-
+    board = utils.get_board()
     snake = Snake()
-    # Board set up
-    spots[0][0] = 1
-    food = find_food(spots)
+    board[0][0] = 1
+    food = utils.generate_food(board)
+    return clock, board, snake, food
 
+def run(game): 
+    clock, board, snake, food = initialize_game()
     while True:
-        clock.tick(15)
+        clock.tick(5*SNAKE_SPEED)
         # Event processing
-        done = False
         events = pygame.event.get()
         for event in events: 
             if event.type == pygame.QUIT:
-                print("Quit given")
-                done = True
-                break
-        if done:
-            return False
-
-        snake.populate_nextDir(events, "arrows")
+                return "done"
+        snake.process_keyboard_events(events)
 
         # Game logic
-        next_head = move(snake)
-        if (end_condition(spots, next_head)):
-            return snake.tailmax
+        next_head = snake.move()
 
-        if is_food(spots, next_head):
-            snake.tailmax += 4
-            food = find_food(spots)
+        if (utils.is_wall(board, next_head)):
+            if utils.play_again(game, snake.score):
+                return run(game)
+            else:             
+                return "done"
+
+        if utils.is_food(board, next_head):
+            snake.increase_score()
+            food = utils.generate_food(board)
 
         snake.deque.append(next_head)
 
         if len(snake.deque) > snake.tailmax:
             snake.deque.popleft()
-
-        # Draw code
-        screen.fill(BLACK)  # makes screen black
-
-        spots = update_board(screen, [snake], food)
-
+        game.fill(utils.BLACK) 
+        board = utils.update_board(game, [snake], food)
         pygame.display.update()
 
-def game_over(screen, eaten):
-    message1 = "You ate %d foods" % eaten
-    message2 = "Press enter to play again, esc to quit."
-    game_over_message1 = pygame.font.Font(None, 30).render(message1, True, BLACK)
-    game_over_message2 = pygame.font.Font(None, 30).render(message2, True, BLACK)
-
-    overlay = pygame.Surface((BOARD_LENGTH * OFFSET, BOARD_LENGTH * OFFSET))
-    overlay.fill((84, 84, 84))
-    overlay.set_alpha(150)
-    screen.blit(overlay, (0,0))
-
-    screen.blit(game_over_message1, (35, 35))
-    screen.blit(game_over_message2, (65, 65))
-    game_over_message1 = pygame.font.Font(None, 30).render(message1, True, WHITE)
-    game_over_message2 = pygame.font.Font(None, 30).render(message2, True, WHITE)
-    screen.blit(game_over_message1, (32, 32))
-    screen.blit(game_over_message2, (62, 62))
-   
-    pygame.display.update()
-
-    while True: 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    return False
-                if event.key == pygame.K_RETURN:
-                    return True
-
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode([BOARD_LENGTH * OFFSET,
-        BOARD_LENGTH * OFFSET])
-    pygame.display.set_caption("Snake")
-    thing = pygame.Rect(10, 10, 50, 50)
-    pygame.draw.rect(screen,pygame.Color(255,255,255,255),pygame.Rect(50,50,10,10))
-    first = True
-    playing = True
-    while playing:
-        if first:
-            menu(screen)
-        now = one_player(screen)
-        if now == False:
-            break
-        else:
-            eaten = now / 4 - 1
-            playing = game_over(screen, eaten)
-            first = False
+    game = utils.initialize_board()
+    if utils.start_screen(game):
+        run(game)
     pygame.quit()
 
 if __name__ == "__main__":
